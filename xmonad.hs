ac0aa81f8c6c5d49ab524cf836c2c0498842ad69
Joshua Hanson <nibblebot@gmail.com>
2013-07-17 11:29:14 -0400

    xmonad/conky updates

--- xmonad.hs
+++ xmonad.hs
@@ -15,6 +15,8 @@ import XMonad.Config.Desktop
  
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
+import Data.Monoid
+
 import Graphics.X11.Xlib
 import Graphics.X11.ExtraTypes.XF86
 import qualified XMonad.StackSet as W
@@ -25,7 +27,7 @@ import System.Exit
 main = do
     wsBar <- spawnPipe myWorkSpaceBar
     statusBar <- spawnPipe myStatusBar
-    xmonad $ desktopConfig
+    xmonad $ ewmh desktopConfig
         { terminal = myTerminal
         , manageHook = manageDocks <+> myManageHook <+> manageHook desktopConfig
         , layoutHook = myLayoutHook
@@ -38,11 +40,11 @@ main = do
         , workspaces = myWorkspaces
         }
         `additionalKeysP` myKeysP
-        {-`additionalKeys`-}
-        {-[ ((0, xF86XK_AudioLowerVolume   ), spawn "/home/josh/bin/dvol lower")-}
-        {-, ((0, xF86XK_AudioRaiseVolume   ), spawn "/home/josh/bin/dvol raise")-}
-        {-, ((0, xF86XK_AudioMute          ), spawn "/home/josh/bin/dvol toggle")-}
-        {-]-}
+        `additionalKeys`
+        [ ((0, xF86XK_AudioLowerVolume), spawn "/home/josh/bin/dvol lower")
+        , ((0, xF86XK_AudioRaiseVolume), spawn "/home/josh/bin/dvol raise")
+        , ((0, xF86XK_AudioMute), spawn "/home/josh/bin/dvol toggle")
+        ]
 
 myKeysP = 
     [ ("C-M-q", spawn "killall conky dzen2; xmonad --recompile && xmonad --restart")
@@ -54,60 +56,55 @@ myTerminal = "urxvtc"
 
 myLogHook h = dynamicLogWithPP ( defaultPP
 	{
-		  ppCurrent		= dzenColor deepred background .	pad
-		, ppVisible		= dzenColor foreground background . 	pad
-		, ppHidden		= dzenColor aqua background . 	pad
-		, ppHiddenNoWindows	= dzenColor grey background.	pad
+		  ppCurrent		= dzenColor orange background
+		, ppVisible		= dzenColor orange background
+		, ppHidden		= dzenColor white background
+		, ppHiddenNoWindows	= dzenColor grey background
 		, ppWsSep		= ""
-		, ppSep			= "    "
-		{-, ppLayout		= wrap "^ca(1,xdotool key alt+space)" "^ca()" . dzenColor white1 background .-}
-				{-(\x -> case x of-}
-                    {-"ResizableTall" -> "Tall"-}
-                    {-"Mirror ResizableTall" -> "Wide"-}
-                    {-"Full" -> "Full"-}
-                    {-[>"ResizableTall" -> wrapBitmap "rob/tall.xbm"<]-}
-                    {-[>"Mirror ResizableTall" -> wrapBitmap "rob/mtall.xbm"<]-}
-                    {-[>"Full" -> wrapBitmap "rob/full.xbm"<]-}
-				{-) -}
---		, ppTitle	=   wrap "^ca(1,xdotool key alt+shift+x)^fg(#222222)^i(/home/sunn/.xmonad/dzen2/corner_left.xbm)^bg(#222222)^fg(#AADB0F)^fn(fkp)x^fn()" "^fg(#222222)^i(/home/sunn/.xmonad/dzen2/corner_right.xbm)^ca()" .  dzenColor white0 "#222222" . shorten 40 . pad		
-		, ppOrder	=  \(ws:l:t:_) -> [ws,l]
+		, ppSep			= ""
+		, ppOrder	=  \(ws:l:t:_) -> [ws]
 		, ppOutput	=   hPutStrLn h
 	})
 
 {-wrapBitmap bitmap = "^p(5)^i(" ++ myBitmapsPath ++ bitmap ++ ")^p(5)"-}
 
 -- Border
-myBorderWidth = 0
-myInactiveBorderColor = "#000000"
-myActiveBorderColor = "#5E8D87"
+myBorderWidth = 1
+myInactiveBorderColor = "#999999"
+myActiveBorderColor = "#FFD10B"
 
 -- Bitmaps
 myBitmapsPath = "/home/josh/.icons/"
 
 -- Font
-myFont = "Monaco:size=10"
-{-myFont = "-*-PixelCarnageMono-*-*-*-*-11-*-*-*-*-*-*-*"-}
-{-myFont = "-*-nu-*-*-*-*-*-*-*-*-*-*-*-*"-}
-{-myFont = "-*-dina-*-*-*-*-11-*-*-*-*-*-*-*"-}
-{-myFont = "-*-terminus-*-*-*-*-12-*-*-*-*-*-iso8859-*"-}
+myFont = "Consolas:size=9"
 
-{-myFont = "-*-proggyclean-*-*-*-*-13-*-*-*-*-*-*-*"-}
 -- Colors
-foreground = "#000000"
-background = "#D2E4FC"
 aqua = "#036654"
 deepred = "#A80000"
-grey = "#999999"
+hotpink = "#DF5793"
+orange = "#FFA916"
+white = "#FFFFFF"
+grey = "#AAAAAA"
+solar_light = "#FDF6E3"
+lightgrey = "#333333"
+
+foreground = "#DDDDDD"
+background = lightgrey
 
 -- dzen general options
 -- 
-myDzenGenOpts = "-fg '" ++ foreground ++ "' -bg '" ++ background ++ "' -h '20'" ++ " -fn '" ++ myFont ++ "'"
+myDzenGenOpts = "-fg '" ++ foreground ++ "' -bg '" ++ background ++ "' -h '14'" ++ " -fn '" ++ myFont ++ "'"
  
-myWorkSpaceBar = "dzen2 -e '' -w 520 -x 0 -y 0 -ta l " ++ myDzenGenOpts
-myStatusBar = "conky -c ~/.conkyrc | dzen2 -e '' -x 965 -y 0 -w 475 -ta r " ++ myDzenGenOpts
+myWorkSpaceBar = "dzen2 -e '' -w 250 -x 0 -y 0 -ta l " ++ myDzenGenOpts
+myStatusBar = "conky -qc ~/.conkyrc | dzen2 -e '' -x 250 -y 0 -w 1670 -ta r " ++ myDzenGenOpts
  
 -- Layouts
-myLayoutHook = avoidStruts $ smartBorders $ smartSpacing 6 $ (tiled ||| Mirror tiled ||| Full)
+myLayoutHook = 
+  avoidStruts $ 
+  smartBorders $ 
+  {-smartSpacing 6 $ -}
+  (tiled ||| Mirror tiled ||| Full)
   where
     tiled = ResizableTall nmaster delta ratio []
     nmaster = 1
@@ -116,7 +113,7 @@ myLayoutHook = avoidStruts $ smartBorders $ smartSpacing 6 $ (tiled ||| Mirror t
  
 {-Workspaces-}
 myWorkspaces            :: [String]
-myWorkspaces            = clickable . (map dzenEscape) $ ["web","vim","term","chat","music","vm","7","8","9"]
+myWorkspaces            = clickable . (map dzenEscape) $ [" main "," vim "," term "," chat "," music "," vm ", " read "]
   where clickable l     = [ "^ca(1,xdotool key alt+" ++ show (n) ++ ")" ++ ws ++ "^ca()" |
                             (i,ws) <- zip [1..] l,
                             let n = i ]
